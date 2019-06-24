b0871f2956bb1d0b3eef9007d870dd667c934989
Machine100 <joesmileyyy@yahoo.com>
2019-06-24 16:50:15 -0400

    .

--- firesyncclient/public/login.js
+++ firesyncclient/public/login.js
@@ -1,39 +1,28 @@
-// get the button element from html and place into the DOM - Instantiate it as a vairable
-//this how we ___
-const btnasvar = document.getElementById('btnLogin');
+const loginForm = document.querySelector('#login-form'); // why are these elements selected differently?
+const loginbutton = document.getElementById('loginButton'); // why is the button element camelcase and the form element kebob case?
+const createForm = document.querySelector('#create-form'); // why is this variable camel case,
+const createbutton = document.getElementById('createButton'); // and this one is lowercase?
 
-//this is how we querry elements from the DOM 
-const signupForm = document.querySelector('#signup-form');
-//add an event listener to the form
-signupForm.addEventListener('submit', e => {
+loginForm.addEventListener('submit', e => {
 	 e.preventDefault();
-	 //getuserinfo
-	 const email = signupForm['signup-email'].value;
-	 const password = signupForm['signup-password'].value;
-	 console.log(email, password)
-})
+	 const email = loginForm['login-email'].value;
+	 const password = loginForm['login-password'].value;
+}) 
 
-
-
-// now that we have the element, attach a click event to it.
-btnasvar.addEventListener('click', e => {
-	//get email and pw
-//	const email = txtEmail.value;
-//	const pass = txtPassword.value;
-
-//  We ae going to use methods attached to this constant:
-	const auth = firebase.auth(); //makes an auth object. Can use that object to communicate. Same as s3=boto3('s3') does.
-	const db = firebase.firestore();
-
+loginbutton.addEventListener('click', e => {
 	console.log( e.type);
-	//sign in
-	const promise = auth.signInWithEmailAndPassword("aa@aa.com", "aaaaaa");
-	// this returns a promise. Use this to catch errors.
-	// the promise will resolve into
+	const promise = auth.signInWithEmailAndPassword(email, password);
 	promise.catch(e => console.log(e.message));
-
 })
 
- 
-// Variables are not just a value.
- //Variables can be an instantion of the class.
+createForm.addEventListener('submit', e => {
+	 e.preventDefault();
+	 const email = createForm['create-email'].value;
+	 const password = createForm['create-password'].value;
+}) 
+
+createbutton.addEventListener('click', e => {
+	console.log( e.type);
+	const promise = auth.createUserWithEmailAndPassword(email, password);
+	promise.catch(e => console.log(e.message));
+})
