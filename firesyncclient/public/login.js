82dcbf87f2a74e6f8173afe9309e4a8b45c85b35
Machine100 <joesmileyyy@yahoo.com>
2019-06-26 09:14:54 -0400

    .

--- firesyncclient/public/login.js
+++ firesyncclient/public/login.js
@@ -1,28 +1,44 @@
 const loginForm = document.querySelector('#login-form'); // why are these elements selected differently?
 const loginbutton = document.getElementById('loginButton'); // why is the button element camelcase and the form element kebob case?
-const createForm = document.querySelector('#create-form'); // why is this variable camel case,
+const createForm = document.querySelector('#create-form'); // why is this variable kabob case,
 const createbutton = document.getElementById('createButton'); // and this one is lowercase?
+const logoutbutton = document.getElementById('logoutButton');
+const consolecreds = document.getElementById('consoleCreds');
+var userInfo;
 
 loginForm.addEventListener('submit', e => {
 	 e.preventDefault();
-	 const email = loginForm['login-email'].value;
-	 const password = loginForm['login-password'].value;
 }) 
 
 loginbutton.addEventListener('click', e => {
-	console.log( e.type);
-	const promise = auth.signInWithEmailAndPassword(email, password);
+	const email = loginForm['login-email'].value;
+	const password = loginForm['login-password'].value;
+	console.log("from button listener")
+	console.log(email,password)
+	const promise = auth.signInWithEmailAndPassword(email, password).then(cred => {
+		console.log(cred);
+		userInfo = cred.user;
+		console.log(userInfo)
+	})
 	promise.catch(e => console.log(e.message));
 })
 
 createForm.addEventListener('submit', e => {
 	 e.preventDefault();
-	 const email = createForm['create-email'].value;
-	 const password = createForm['create-password'].value;
 }) 
 
 createbutton.addEventListener('click', e => {
-	console.log( e.type);
-	const promise = auth.createUserWithEmailAndPassword(email, password);
+	const email2 = createForm['signup-email'].value;
+	const password2 = createForm['signup-password'].value;
+	const promise = auth.createUserWithEmailAndPassword(email2, password2);
 	promise.catch(e => console.log(e.message));
+})
+
+logoutbutton.addEventListener('click', e => {
+	e.preventDefault();
+	auth.signOut();
+})
+
+consolecreds.addEventListener('click', e => {
+	console.log (userInfo)
 })
