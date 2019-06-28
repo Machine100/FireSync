f0b020cad0fd3d29e878c2995e3e782f2fe73997
Machine100 <joesmileyyy@yahoo.com>
2019-06-28 08:38:24 -0400

    .

--- firesyncclient/src/App.js
+++ firesyncclient/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
 import AuthBar from './components/AuthBar'
-
+import StyledForm from './components/StyledForm'
 
 
 //document.addEventListener("DOMContentLoaded", event => {const app = firebase.app()})
 
 //firebase.initializeApp(firebaseConfig);
 
-
+//
 
 function App() {
   return (
     <div className="App">
  		<AuthBar />
+ 		<StyledForm />
+
+//
     </div>
   );
 }
---
+++ firesyncclient/src/components/StyledForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Button';
+
+
+const StyledForm = () => {
+  return (
+  
+    <div>          
+ 
+ 		<AppBar postion="static">
+ 			<Toolbar>
+ 				<Typography variant="title" color="inherit">
+ 					React kdkdkdkdkd
+ 				</Typography>
+			</Toolbar>
+		</AppBar>
+
+
+
+	</div>
+  );
+}
+
+export default StyledForm;
