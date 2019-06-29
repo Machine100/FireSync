5455d8bb67fb14c49ad664d79e3cd551da76042a
Machine100 <39662431+Machine100@users.noreply.github.com>
2019-06-29 14:49:44 -0400

    .

--- firesyncclient/src/components/StyledForm.js
+++ firesyncclient/src/components/StyledForm.js
@@ -2,8 +2,13 @@ import React from 'react';
 import styled from 'styled-components'
 
 const Button = styled.button`
-  background: ${props => (props.primary ? 'black' : 'white')};
-  color: ${props => (props.primary ? 'white' : 'black')};
+  background: ${props => (props.primary ? 'garnet' : 'gold')};
+  color: ${props => (props.primary ? 'gold' : 'garnet')};
+`
+
+const Form = styled.form`
+  background: ${props => (props.primary ? 'garnet' : 'gold')};
+  color: ${props => (props.primary ? 'gold' : 'garnet')};
 `
 
 function StyledForm () {
@@ -12,8 +17,15 @@ function StyledForm () {
     <div>          
  
     <Button>A normal button</Button>
-    <Button>A normal button</Button>
     <Button primary>The primary button</Button>
+
+    <Form primary>
+      First name:
+      <input type="text" name="firstname"></input><br />
+      Last name:
+      <input type="text" name="lastname"></input>
+    </Form>
+
     
 
 
